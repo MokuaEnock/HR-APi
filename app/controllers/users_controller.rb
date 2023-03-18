3098d9fb030408ed35abbae9f41e56221c721f4c
Enock <enock.mokua@student.moringaschool.com>
2023-03-18 21:15:37 +0300

    add jwt gem to the system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def authenticate
+    user = User.find_by(email: paras[:email])
+    if user && authenticate(params[:password])
+      #authentication successful
+    else
+      #authentication failed
+    end
+  end
+
 end

00b5701b8273ffa12eb54e191d5b9286d0b24304
Enock <enock.mokua@student.moringaschool.com>
2023-03-18 21:22:50 +0300

    create token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+  def create
+  user = User.find_by(email: params[:email])
+if user && user.authenticate(paramsp[:password])
+  jwt = encode_token(user_id: user.id)
+  render json: {jwt: jwt}
+else
+  render json: {error: "Invalid email or password"}, status: :unprocessable_entity
+end
+  end
+
+  private
+
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
 end
